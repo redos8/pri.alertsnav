8812ac14069017f235976ef913b3edcd80059c83
Yuriy Petrov <yuri.petrov@haymarket.in.ua>
2017-04-12 18:04:24 +0300

    Update es5

--- alertsnav.es5.js
+++ alertsnav.es5.js
@@ -77,7 +77,7 @@ var AlertsNav = function () {
 
 			// Scroll to top when we're loaded
 			// it fixing offset's bug
-			if (scrollBack && !('ontouchstart' in window || navigator.msMaxTouchPoints)) {
+			if (scrollBack && !('ontouchstart' in window || navigator.msMaxTouchPoints) && (window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1)) {
 				$(window).scrollTop(0);
 			}
 
